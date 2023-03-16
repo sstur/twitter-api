323bdd0ad30d6e0cb2e14561a1111dc8e25dc219
Simon <sstur@me.com>
2023-03-15 22:55:34 -0700

    Add endpoint /api/users/me

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,26 @@
 import type { Application } from 'express';
 
 import { db } from '../db';
+import { authenticate } from './helpers/auth';
 
 export default (app: Application) => {
   app.get('/api/users', async (request, response, next) => {
     try {
       const users = await db.User.getAll();
       response.json(users);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  app.get('/api/users/me', async (request, response, next) => {
+    try {
+      const user = await authenticate(request.header('authorization') ?? '');
+      if (!user) {
+        response.status(401).json({ success: false, error: 'Unauthorized' });
+        return;
+      }
+      response.json(user);
     } catch (error) {
       next(error);
     }
